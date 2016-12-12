99b7c08f4e04d8d44e099108d9f0deb2296bef48
wakwaleed <waleedgraphy13@gmail.com>
2016-12-12 21:55:14 +0500

    web project necessary files

---
+++ web_project/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Graphypro</title>
+
+	<!-- Stylesheet --><!-- 
+	<link rel="stylesheet" type="text/css" href="css/style.css" /> -->
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="body">
+
+	<header class="mainHeader">
+
+		<!-- Logo -->
+		<img src="images/logo.png" >
+
+		<!-- Navigation -->
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Blog</a></li>
+			</ul>
+		</nav><!-- /nav -->
+	</header><!-- /mainHeader -->
+
+
+	<div class="mainContent">
+
+		<div class="content">
+			<article class="topContent">
+				<header>	
+					<h2><a href="#" title="firstPost" alt="firstPost">first post</a></h2>
+				</header>
+				<footer>
+					<p class="postInfo">This is written by <a href="#">waleed</a></p>
+				</footer>
+				<content>
+					<p>Some lorem ipsum text here Some here Some lorem ipsum text herehere Some lorem <br>ipsum text herehere Some lorem ipsum text herehere <br>Some lorem ipsum text herehere Some lorem ipsum text <br>herehere Some lorem ipsum text here
+						lorem ipsum text hereSbr	<br>ome lorem ipsum text here Some lorem ipsum text herehere</p>
+				</content>
+			</article><!-- /topContent -->
+
+			<article class="topContent">
+				<header>	
+					<h2><a href="#" title="firstPost" alt="firstPost">first post</a></h2>
+				</header>
+				<footer>
+					<p class="postInfo">This is written by <a href="#">waleed</a></p>
+				</footer>
+				<content>
+					<p>Some lorem ipsum text here Some here Some lorem ipsum text herehere Some lorem <br>ipsum text herehere Some lorem ipsum text herehere <br>Some lorem ipsum text herehere Some lorem ipsum text <br>herehere Some lorem ipsum text here
+						lorem ipsum text hereSbr	<br>ome lorem ipsum text here Some lorem ipsum text herehere</p>
+				</content>
+			</article><!-- /topContent -->
+
+			<article class="topContent">
+				<header>	
+					<h2><a href="#" title="firstPost" alt="firstPost">first post</a></h2>
+				</header>
+				<footer>
+					<p class="postInfo">This is written by <a href="#">waleed</a></p>
+				</footer>
+				<content>
+					<p>Some lorem ipsum text here Some here Some lorem ipsum text herehere Some lorem <br>ipsum text herehere Some lorem ipsum text herehere <br>Some lorem ipsum text herehere Some lorem ipsum text <br>herehere Some lorem ipsum text here
+						lorem ipsum text hereSbr	<br>ome lorem ipsum text here Some lorem ipsum text herehere</p>
+				</content>
+			</article><!-- /topContent -->
+
+
+		</div><!-- /content -->
+
+
+
+
+<!-- Sidebar section starts here -->
+	<aside class="topSidebar">
+			<article>
+				<h2>Top Sidebar</h2>
+				<img src="images/iframe.jpg" width="230px" height="240px" />
+			</article>
+	</aside><!-- /topSidebar -->
+
+		<aside class="middleSidebar">
+			<article>
+				<h2>Middle Sidebar</h2>
+				<img src="images/iframe.jpg" width="230px" height="240px" />
+			</article>
+	</aside><!-- /middleSidebar -->
+
+
+	<aside class="bottomSidebar">
+
+			<article>
+				<h2>Bottom Sidebar</h2>
+				<img src="images/iframe.jpg" width="230px" height="240px" />
+			</article>
+
+	</aside><!-- /bottomSidebar -->
+
+
+
+	<footer class="mainFooter">
+		<p>Copyright &copy; 2016 <a href="#">Graphypro</a></p>
+	</footer>
+
+
+	</div><!-- /mainContent -->
+
+</body>
+</html>
+
+<?php  ?>
